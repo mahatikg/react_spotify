046ae14b019ff28cd1991baa58a780a5bf3019ba
jaspercurry <jaspercurry@gmail.com>
2016-10-04 17:32:07 -0400

    made basic artist show component

---
+++ src/components/artist_show.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import{connect} from 'react-redux'
+
+function ArtistShow(props) {
+
+  return(
+    <div>
+      <h1>Artistttttt</h1>
+
+    </div>
+
+  )
+
+}
+
+export default ArtistShow;
+
+// function mapStateToProps(state, ownProps) {
+// const user = state.users.find((user) => {return user.id == ownProps.params.id})
+// return {user: user}
+// }
+//
+// const componentCreator = connect(mapStateToProps)
+// export default componentCreator(UserShow);
