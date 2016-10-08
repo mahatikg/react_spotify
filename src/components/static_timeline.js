6881a9a2bad9aa8ff73739fc9d094f27d131fd91
Clifton Regis <github email address>
2016-10-08 13:28:09 -0400

    refactored timeline code

--- src/components/static_timeline.js
+++ src/components/static_timeline.js
@@ -7,10 +7,30 @@ require('react-image-timeline/dist/timeline.css');
 class SpotifyTimelineComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.events = props.user.events
+  
+ this.events = [
+      {
+              "date": Date.parse("2013-05-15T07:00:00.000Z"),
+              "text": "Your Top Artist for the past month",
+              "title": this.props.user.short_term.artists[0].name,
+              "imageUrl": this.props.user.short_term.artists[0].image
+    },
+    {
+              "date": Date.parse("2013-05-15T07:00:00.000Z"),
+              "text": "Your Top Artist for the past 3 months",
+              "title": this.props.user.mid_term.artists[0].name,
+              "imageUrl": this.props.user.mid_term.artists[0].image
+    },
 
+    {
+              "date": Date.parse("2013-08-14T07:00:00.000Z"),
+              "text": "Your Top all time artist",
+              "title": this.props.user.long_term.artists[0].name,
+              "imageUrl": this.props.user.long_term.artists[0].image
+    }
+    ]
 
-  }
+}
 
   render() {
     return ( <div> <br/><h2>TimeLine</h2>
