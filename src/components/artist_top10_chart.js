0cb1a257d447acc2730bafe05a4e3e01e0c94765
daniellasheva <daniellasheva@gmail.com>
2016-10-10 19:21:29 -0400

    resolved Bar component to time periods

--- src/components/artist_top10_chart.js
+++ src/components/artist_top10_chart.js
@@ -6,10 +6,13 @@ import {Bar} from 'react-chartjs-2';
 class BarComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.labels = props.data["artists"].slice(0, 10).map( artist => { return (artist.name)})
-    this.art_pop = props.data["artists"].slice(0, 10).map( artist => { return parseInt(artist.popularity)} )
+    this.state={
+      term: "mid_term"
+    }
+
     this.options = {maintainAspectRatio: false}
     this.data = this.data.bind(this)
+    this.displayBar= this.displayBar.bind(this)
     this.term = props.term
   }
 
@@ -24,18 +27,71 @@ class BarComponent extends React.Component {
         borderWidth: 1,
         hoverBackgroundColor: 'rgba(255,99,132,0.4)',
         hoverBorderColor: 'rgba(255,99,132,1)',
-        data: this.art_pop,
+        data: this.dataset,
       }
     ]
   })
   }
 
 
+  displayBar(){
+    if (this.state.term== "short_term"){
+
+      this.labels = this.props.data.short_term.artists.slice(0, 10).map( artist => { return (artist.name)})
+      this.dataset = this.props.data.short_term.artists.slice(0, 10).map( artist => { return parseInt(artist.popularity)})
+
+      return(
+        <div>
+          <Bar data={this.data()} width={100} height={600} options={this.options}/>
+        </div>
+      )
+    } if (this.state.term =="mid_term"){
+      this.labels = this.props.data.mid_term.artists.slice(0, 10).map( artist => { return (artist.name)})
+      this.dataset = this.props.data.mid_term.artists.slice(0, 10).map( artist => { return parseInt(artist.popularity)})
+
+      return(
+        <div>
+          <Bar data={this.data()} width={100} height={600} options={this.options}/>
+        </div>
+      )
+
+    } if (this.state.term =="long_term"){
+      this.labels = this.props.data.long_term.artists.slice(0, 10).map( artist => { return (artist.name)})
+      this.dataset = this.props.data.long_term.artists.slice(0, 10).map( artist => { return parseInt(artist.popularity)})
+
+
+      return(
+        <div>
+          <Bar data={this.data()} width={100} height={600} options={this.options}/>
+        </div>
+      )
+
+    }
+  }
+
+  clickHandler(term){
+    console.log(term)
+    this.setState({
+      term: term
+    })
+  }
 render() {
-  return ( <div> <br/> <br/> <h2>Top 10 {this.term} Artists</h2>
+  return (
     <div>
-      <Bar data={this.data()} width={100} height={600} options={this.options}/>
-    </div>
+      <div className="btn-group btn-group-justified" role="group" aria-label="...">
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-default" onClick={()=>this.clickHandler("long_term")}>As Long Ago As You Can Remember</button>
+          </div>
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-default" onClick={()=>this.clickHandler("mid_term")}>Four Months Ago</button>
+          <br/></div>
+          <div className="btn-group" role="group">
+            <button type="button" className="btn btn-default" onClick={()=>this.clickHandler("short_term")}>Four Weeks Ago</button>
+          <br/></div>
+      </div>
+
+    <br/> <br/> <h2>Top 10 {this.term} Artists</h2>
+    {this.displayBar()}
   </div>)
 }
 
--- src/components/user_genre_pie.js
+++ src/components/user_genre_pie.js
@@ -6,13 +6,60 @@ import {Pie} from 'react-chartjs-2';
 class PieComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.labels = Object.keys( props.data.genres)
-    this.dataset = this.labels.map(function(values){return props.data.genres[values]})
+    debugger
+    this.state={
+      term: "mid_term"
+    }
+
     this.options = {maintainAspectRatio: false, animateRotate: true, animationEasing: "easeOutCirc", animationSteps: 150, onAnimationComplete : null }
     // this.pieOptions = {animation: true}
     // this.options = {maintainAspectRatio: false, segmentShowStroke: true, animation: true, animationSteps: 150, animationEasing: "easeOutCirc", animateRotate: true, onAnimationComplete : null}
-    this.term= props.term
+    this.term= this.props.term
+    this.displayPie= this.displayPie.bind(this)
+
   }
+
+  displayPie(){
+    if (this.state.term== "short_term"){
+
+      this.labels = Object.keys( this.props.data.short_term.genres)
+      this.dataset = this.labels.map((values)=>{return this.props.data.short_term.genres[values]})
+
+      return(
+        <div id="pie_size">
+          <Pie data={this.data()} width={200} height={200} options={this.options} />
+        </div>
+      )
+    } if (this.state.term =="mid_term"){
+      this.labels = Object.keys( this.props.data.mid_term.genres)
+      this.dataset = this.labels.map((values)=>{return this.props.data.mid_term.genres[values]})
+
+      return(
+        <div id="pie_size">
+          <Pie data={this.data()} width={200} height={200} options={this.options} />
+        </div>
+      )
+
+    } if (this.state.term =="long_term"){
+      this.labels = Object.keys( this.props.data.long_term.genres)
+      this.dataset = this.labels.map((values)=>{return this.props.data.long_term.genres[values]})
+
+      return(
+        <div id="pie_size">
+          <Pie data={this.data()} width={200} height={200} options={this.options} />
+        </div>
+      )
+
+    }
+  }
+
+  clickHandler(term){
+    console.log(term)
+    this.setState({
+      term: term
+    })
+  }
+
 
 
 
@@ -52,11 +99,22 @@ class PieComponent extends React.Component {
           }
 
 render() {
-  return ( <div> <br/><h2>{this.term} Breakdown of Genres</h2>
+  return (
     <div>
-      <div id="pie_size">
-        <Pie data={this.data()} width={200} height={200} options={this.options} />
-      </div>
+        <div className="btn-group btn-group-justified" role="group" aria-label="...">
+            <div className="btn-group" role="group">
+              <button type="button" className="btn btn-default" onClick={()=>this.clickHandler("long_term")}>As Long Ago As You Can Remember</button>
+            </div>
+            <div className="btn-group" role="group">
+              <button type="button" className="btn btn-default" onClick={()=>this.clickHandler("mid_term")}>Four Months Ago</button>
+            <br/></div>
+            <div className="btn-group" role="group">
+              <button type="button" className="btn btn-default" onClick={()=>this.clickHandler("short_term")}>Four Weeks Ago</button>
+            <br/></div>
+        </div>
+    <br/><h2>{this.term} Breakdown of Genres</h2>
+    <div>
+      {this.displayPie()}
     </div>
   </div>)
 }
