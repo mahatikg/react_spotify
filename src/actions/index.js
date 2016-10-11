b1d369c2510050496b5f5c7eaca6a66faf6ce85a
jaspercurry <jaspercurry@gmail.com>
2016-10-11 18:45:52 -0400

    changed url

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,14 +12,14 @@
 
 export function fetchUsers() {
 
-  const users = fetch('http://rocky-dusk-25774.herokuapp.com/users').then(response => { return response.json()}).then(userPayLoad => { return userPayLoad})
+  const users = fetch('https://rocky-dusk-25774.herokuapp.com/users').then(response => { return response.json()}).then(userPayLoad => { return userPayLoad})
   return { type: 'FETCH_USERS',
             payload: users
           }
 }
 
 export function compareUsers(id1, id2){
-  const comparedusers = fetch(`http://rocky-dusk-25774.herokuapp.com/compare?id1=${id1}&id2=${id2}`).then(response => { return response.json()}).then(userPayLoad => { return userPayLoad})
+  const comparedusers = fetch(`https://rocky-dusk-25774.herokuapp.com/compare?id1=${id1}&id2=${id2}`).then(response => { return response.json()}).then(userPayLoad => { return userPayLoad})
   return { type: 'COMPARE_USERS',
             payload: comparedusers
           }
