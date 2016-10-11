29f9fae5117ad757544a5d88f6bce55090c61154
jaspercurry <jaspercurry@gmail.com>
2016-10-11 17:02:57 -0400

    modified git ignore file

---
+++ src/components/home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './nav_bar';
+import {Link} from 'react-router';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="home">
+
+              <h1>STASTE</h1>
+                <h4><a href="https://accounts.spotify.com/authorize/?client_id=031ed6ea90bd4727b184cd84219dd697&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5050%2Fcallback%2F&scope=user-top-read">Login with Spotify!</a></h4>
+                {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ import BarComponent1 from './components/artist_top10_chart'
 
 export default (
   <Route path='/' component={App}>
-   <Route path='/users' component={UserIndex}/>
+   <Route path='/users' component={UserIndex} />
    <Route path='/users/:id' component={UserShow}>
      <Route path='/users/:id' component={ArtistShow} />
      <Route path='/users/:id' component={BarComponent1} />
