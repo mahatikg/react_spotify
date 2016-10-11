0b0b797419fcf8165674a510f518a6ed816be144
jaspercurry <jaspercurry@gmail.com>
2016-10-10 20:26:39 -0400

    nearly done

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar title="Staste" url='/' />
-        <Link to="/users">See all users</Link>
-        {this.props.children}
+        <div className="home">
+
+              <h1>STASTE</h1>
+                <h4><a href="https://accounts.spotify.com/authorize/?client_id=031ed6ea90bd4727b184cd84219dd697&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5050%2Fcallback%2F&scope=user-top-read">Login with Spotify!</a></h4>
+                {this.props.children}
+        </div>
       </div>
     );
   }
