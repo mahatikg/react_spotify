179911eacce21dc935b0f161172e14987e72ecff
jaspercurry <jaspercurry@gmail.com>
2016-10-11 21:04:43 -0400

    modified timeline to be a local component

--- src/components/static_timeline.js
+++ src/components/static_timeline.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import {Pie} from 'react-chartjs-2';
-import Timeline from 'react-image-timeline';
-require('react-image-timeline/dist/timeline.css');
+import Timeline from './timeline/timeline';
+require('./timeline/timeline.css');
 
 class SpotifyTimelineComponent extends React.Component {
   constructor(props) {
